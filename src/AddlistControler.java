3c945f4fb53a2e22dd63b9dd6513ca354ff6b755
michelebenveuto <35434145+michelebenveuto@users.noreply.github.com>
2018-08-21 12:40:27 -0600

    empezando el boton de agregar

---
+++ src/AddlistControler.java
@@ -0,0 +1,11 @@
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class AddlistControler {
+    @FXML
+    Label nameLabel;
+
+    @FXML
+    javafx.scene.control.TextField nameInput;
+}
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,24 @@
 package sample;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.cell.PropertyValueFactory;
-import product.*;
+import javafx.stage.Stage;
 import lists.List;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 
+import java.io.IOException;
+
+
 public class Controller {
+    @FXML
+    Button addListButton;
     @FXML
     TableView<List> listsTable;
 
@@ -24,10 +34,9 @@ public class Controller {
     @FXML
     TableColumn estimateColumn;
 
+    ObservableList<List> listas =FXCollections.observableArrayList();
+
     public void initialize(){
-        ObservableList<List> listas =FXCollections.observableArrayList(
-                new List("Super", "17/08/2018")
-        );
 
         nameColumn.setCellValueFactory(
                 new PropertyValueFactory<List,String>("name")
@@ -36,5 +45,19 @@ public class Controller {
                 new PropertyValueFactory<List,String>("date")
         );
         listsTable.setItems(listas);
+    }
+
+    public void addList(ActionEvent event){
+        Parent root;
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("addList.fxml"));
+            root=loader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Agregar una nueva lista");
+            stage.setScene(new Scene(root, 450,450));
+            stage.show();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
